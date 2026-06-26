4689174d1be2b37fd6d3291186dd30b0b8224544
hasininandam712 <hasininandam712@gmail.com>
2026-06-26 18:15:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector("nav");
+window.addEventListener("scroll", function(){
+    if(window.scrollY > 50){
+        navbar.classList.add("scrolled");
+    }
+    else{
+        navbar.classList.remove("scrolled");
+    }
+});
